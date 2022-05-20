0a38c834acc5622bdcc6b8995b69a6be8aedddce
GM1WKR <gm1wkr@gmail.com>
2022-05-20 13:34:56 +0100

    initial commit

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+
+# https://github.com/codeclan/sr10_classnotes/blob/main/week_01/weekend_homework/homework_brief.md
+from src.pet_shop import *
+from tests.pet_shop_test import *
+
+if __name__ == '__main__':
+    unittest.main()
