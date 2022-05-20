076baaa9ba719357bce0a2916f0061a7bb43fd40
GM1WKR <gm1wkr@gmail.com>
2022-05-20 17:02:10 +0100

    get_pets_sold test Passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -7,3 +7,5 @@ def get_total_cash(pet_shop):
 def add_or_remove_cash(pet_shop, amount_to_add):
     pet_shop["admin"]["total_cash"] += amount_to_add
 
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
