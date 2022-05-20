57a514c21ab5fd71a4f540d6bc26e2bf07d58847
GM1WKR <gm1wkr@gmail.com>
2022-05-20 17:16:48 +0100

    test_all_pets_by_breed__found Passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -15,3 +15,10 @@ def increase_pets_sold(pet_shop, number_of_pets_sold):
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets_of_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets_of_breed.append(pet)
+    return pets_of_breed
