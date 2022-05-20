ff719c0830a94876aa9072ccbb5d6400fa721c7b
GM1WKR <gm1wkr@gmail.com>
2022-05-20 16:37:55 +0100

    add_or_remove_cash Test Passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -3,3 +3,7 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, amount_to_add):
+    pet_shop["admin"]["total_cash"] += amount_to_add
+
